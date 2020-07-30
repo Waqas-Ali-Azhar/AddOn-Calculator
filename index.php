877e6fd75c042bad22a894b2e026a10a85ba16fa
Waqas Ali <waqas_111@hotmail.com>
2020-07-30 10:25:22 +0500

    created boilerplate for ad-on calculator

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+  </head>
+  <body>
+
+    <h1 class="heading">Add-on Calculator </h1>
+
+    <div class="container">
+
+      <div class="calculator-wrapper">
+      
+
+        <div class="item">
+
+          <div class="left ib">
+            <span class="control">-</span>
+          </div>
+          <div class="center ib">
+            <input type="text" name="price" value="1">
+          </div>
+          <div class="right ib">
+            <span class="control">+</span>
+          </div>
+          <div><label>Apple 1 kg-$4</label></div>
+          
+        </div>
+
+
+        <div class="item">
+
+          <div class="left ib">
+            <span class="control">-</span>
+          </div>
+          <div class="center ib">
+            <input type="text" name="price" value="1">
+          </div>
+          <div class="right ib">
+            <span class="control">+</span>
+          </div>
+          <div><label>Mango 1 kg-$7</label></div>
+          
+        </div>
+
+
+        <div class="total">
+          <label>Total- $</label>
+          <span class="total"></span>
+        </div>
+
+    </div>
+
+    </div> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  </body>
+</html> 
+
+<?php ?>
