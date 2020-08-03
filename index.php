67b764953027cb8b3fc01f2d615384967e3a49c1
Waqas Ali <waqas_111@hotmail.com>
2020-08-03 09:49:10 +0500

    new products created

--- index.php
+++ index.php
@@ -13,34 +13,50 @@
       <div class="calculator-wrapper">
       
 
-        <div class="item">
+        <div class="item 1" data="1">
 
           <div class="left ib">
-            <span class="control">-</span>
+            <span class="control dec">-</span>
           </div>
           <div class="center ib">
-            <input type="text" name="price" value="1">
+            <input type="text" name="qty" value="1" data="4">
           </div>
           <div class="right ib">
-            <span class="control">+</span>
+            <span class="control inc">+</span>
           </div>
           <div><label>Apple 1 kg-$4</label></div>
           
         </div>
 
 
-        <div class="item">
+        <div class="item" data="2">
 
           <div class="left ib">
-            <span class="control">-</span>
+            <span class="control dec">-</span>
           </div>
           <div class="center ib">
-            <input type="text" name="price" value="1">
+            <input type="text" name="qty" value="1" data="7">
           </div>
           <div class="right ib">
-            <span class="control">+</span>
+            <span class="control inc">+</span>
           </div>
           <div><label>Mango 1 kg-$7</label></div>
+          
+        </div>
+
+
+        <div class="item 1" data="3">
+
+          <div class="left ib">
+            <span class="control dec">-</span>
+          </div>
+          <div class="center ib">
+            <input type="text" name="qty" value="1" data="16">
+          </div>
+          <div class="right ib">
+            <span class="control inc">+</span>
+          </div>
+          <div><label>Pineapple 1 kg-$16</label></div>
           
         </div>
 
@@ -54,6 +70,12 @@
 
     </div> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="index.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+          // bind events here  
+      });
+    </script>
   </body>
 </html> 
 
